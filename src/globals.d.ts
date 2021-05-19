68046826aeec365eedb36544a92956013f6b7b67
Luca Hostettler <luca.hostettler@iseag.ch>
2021-05-19 15:45:24 +0200

    PWA Install

---
+++ src/globals.d.ts
@@ -0,0 +1,5 @@
+﻿declare namespace JSX {
+  interface IntrinsicElements {
+    'pwa-install': any;
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,13 @@ import * as serviceWorker from './serviceWorker';
 import { CssBaseline } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 import theme from './theme';
+import '@pwabuilder/pwainstall';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
     <App />
+    <pwa-install>Download</pwa-install>
   </ThemeProvider>,
   document.getElementById('root'),
 );
