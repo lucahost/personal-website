f517f04adb1c74024ab083de05d30d657cf48d25
Luca Hostettler <luca.hostettler@digitecgalaxus.ch>
2024-03-06 14:48:21 +0100

    wip migrate from cra to vite

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import reportWebVitals from "./reportWebVitals";
-import "@pwabuilder/pwainstall";
 
-const container = document.getElementById("root");
+const root = createRoot(document.getElementById("root") as Element); 
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const root = createRoot(container!);
 root.render(<App />);
-
-serviceWorkerRegistration.register();
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
